5501878c8a2bbf50f86ed2af03b0dc5dd764e689
jjhyeonjj <jjh4523a@gmail.com>
2023-05-12 11:37:25 +0900

    Props(properies)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Animal = ({myName, skill, leg}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2> 
+        나는 {myName}입니다. &nbsp;
+        나의 능력은 {skill}입니다. &nbsp;
+        나의 다리는 {leg}개 입니다.
+      </h2>
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <h1>hello, world</h1>
+      <Animal myName="호랑이" skill="달리기" leg="4"></Animal>   
+      <Animal myName="독수리" skill="날기" leg="2"></Animal>   
+      <Animal myName="상어" skill="수영" leg="0"></Animal>   
+    </>
   );
 }
 
-export default App;
